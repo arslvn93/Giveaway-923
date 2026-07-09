3a4478a08ff7f44a694a273e37b6e7b496d75473
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-07-09 14:35:16 -0400

    Update Site

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ const config = {
     "ghlWebhookUrl": "https://services.leadconnectorhq.com/hooks/bJbBMjcW619cJrLUJ3kY/webhook-trigger/3yNsurfrt9oBUtRBuygb",
     "facebookPixelId": "851732683084609",
     "turnstileSiteKey": "",
-    "salesGeniusAppApi": "",
+    "salesGeniusAppApi": "api_749d0b91ac8b41fe15c9b7765e26f44d",
     "followUpBossEmail": "",
     "webhookUrl": "https://n8n.salesgenius.co/webhook/giveawayupdate"
   },
@@ -173,7 +173,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1783113188920_0",
+      "id": "q_auto_1783622113844_0",
       "questionText": "What's your current situation?",
       "options": [
         {
@@ -195,7 +195,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1783113188920_1",
+      "id": "q_auto_1783622113845_1",
       "questionText": "What are you looking to do?",
       "options": [
         {
@@ -221,7 +221,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1783113188920_2",
+      "id": "q_auto_1783622113845_2",
       "questionText": "When are you planning to move?",
       "options": [
         {
